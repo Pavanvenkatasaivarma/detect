627fe20a776536e4cedbfb233c020e93a0d471b0
Pavanvenkatasaivarma <99737899+Pavanvenkatasaivarma@users.noreply.github.com>
2024-12-20 11:14:26 +0530

    Update settings.py

--- object_detection/settings.py
+++ object_detection/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-la^11=m&v3oa-+=u)^nfnn%2w=h_hbs+fiee6k7u9-=z(@fqrl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['detect-3.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
